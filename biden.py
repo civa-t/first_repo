a9134d33b0ae07a39d6c93ef3b4ee6372ede9988
civa-t <civanesar.m@mckl.edu.my>
2025-03-06 01:03:34 +0800

    Initial commit

---
+++ biden.py
@@ -0,0 +1,13 @@
+# this statement display the name
+print("Hi, this is Joe Biden")
+
+age = 45
+
+# to check age of the person
+if(age > 50):
+	print("You are qualified for precidency")
+else:
+	print("Try again, next year")
+
+
+print("Good bye")
